cc82bccf3e0d7a03c529f6865e73b11778c49a8b
bibliothec <mupimupicandy@gmail.com>
2021-09-29 11:03:16 +0900

    add reading variable resistor

--- sousei.ino
+++ sousei.ino
@@ -29,22 +29,34 @@
 CytronMD motor1(PWM_DIR, 3, 4);  // PWM 1 = Pin 3, DIR 1 = Pin 4.
 CytronMD motor2(PWM_DIR, 9, 10); // PWM 2 = Pin 9, DIR 2 = Pin 10.
 
- int interval=0;
- int switch_socket=10;
- int motor_speed=32;
+ int interval=100;  // for chattering prevention
+ int switch_socket=10; 
+ int volume_socket=0;
+ float default_motor_speed=32.0;
 
 // The setup routine runs once when you press reset.
 void setup() {
+  Serial.begin(9600);
   pinMode(switch_socket,INPUT_PULLUP);
 }
 
 
 // The loop routine runs over and over again forever.
 void loop() {
+  int analog_val;
+  float input_volt,motor_speed;
+
+  analog_val=analogRead(volume_socket);
+  input_volt=float(analog_val)*(5.0/1023.0);
+  
+  motor_speed=default_motor_speed*input_volt;             //write motor speed formula
+  
   if(digitalRead(switch_socket)==1){
+    delay(interval);
     motor1.setSpeed(motor_speed);    
     motor2.setSpeed(motor_speed);    
   }else if(digitalRead(switch_socket)==0){
+    delay(interval);
     motor1.setSpeed(motor_speed*-1);    
     motor2.setSpeed(motor_speed*-1);  
   }
